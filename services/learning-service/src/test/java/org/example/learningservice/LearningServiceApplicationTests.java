2c056b8a8d2851f6583b176959718e59ca80923d
antonkhomenko <Ya@mail.com>
2026-02-12 14:05:57 +0300

    Add docker-compose.yml++

---
+++ services/learning-service/src/test/java/org/example/learningservice/LearningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.learningservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
