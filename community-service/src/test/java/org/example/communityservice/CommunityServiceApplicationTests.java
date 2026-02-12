edf928d74d61e25e54f5ae863ff899b6e1aea35b
antonkhomenko <Ya@mail.com>
2026-02-12 13:15:36 +0300

    Initial

---
+++ community-service/src/test/java/org/example/communityservice/CommunityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.communityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
